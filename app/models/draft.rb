4692b4c8373dc9028c7c0d477de2c94220b10b5c
brownman <ofer.shaham@gmail.com>
2011-10-13 14:23:11 +0200

    remove nth element if row is ===

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -1,3 +1,4 @@
 class Draft < ActiveRecord::Base
   belongs_to :compare
+  default_scope :order => 'drafts.id ASC'
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,27 +1,59 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(function() {
- // $("#order_credit_card_number").validateCreditCardNumber();
+    // $("#order_credit_card_number").validateCreditCardNumber();
 
-  //$("#btn_show_as_list").populate_li();
-  $("li").live({
+    //$("#btn_show_as_list").populate_li();
+    $(".menu_split li").live({
 
-  click: function() {
+        click: function() {
 
-           $(this).before("<li>=</li>");
+                   $(this).before("<li>=</li>");
 
-           str = $(this).closest(".me").attr('data-id')
-  //$("input").val(str)
-  var select =  $(this).closest(".me");
-var content = select.find("li").text()
-  console.log(select);
-select.find("input").val(content);
+
+  var index = $(".menu_split li").index(this);
+  
+ str = $(".menu_split")
+
+
+                   alert(index)
+                   console.log(str)
+                   //              str = $(this).closest(".me").attr('data-id')
+//find element in current scope
+                   var select =  $(this).closest(".me");
+
+
+                   var content = select.find("li").text()
+
+
+
+
+
+        select.find("input").val(content);
+                   
+
+
+var nth = "li:nth-child(" + index + ")"
+var str = $(".menu_split").find(nth).text()
+
+alert(str)
+if(str=="===")
+{
+
+
+
+$(".menu_split").find(nth).remove()
+}
+
+
+               }
+
+
+    });  
+});
+//   console.log(select);
+//$("input").val(str)
 //               $(select("input")).val("x");
 //        select.("input").text(str);
 
 
-         }
-
-
-});  
-});
